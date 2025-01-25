145ccf1432a3cc3da95bb8a4c29ddb4940172da3
LeThiHongMinh <hongminhle2005@gmail.com>
2025-01-25 17:22:48 +0800

    Level-2

--- src/main/java/Neura.java
+++ src/main/java/Neura.java
@@ -5,6 +5,10 @@ public class Neura {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Store tasks
+        String[] tasks = new String[100];  // Array to store up to 100 tasks
+        int taskCount = 0;  // Keep track of how many tasks are stored
+
         // Display initial greeting
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Neura");
@@ -23,10 +27,27 @@ public class Neura {
                 break;
             }
 
-            // Echo the user input
-            System.out.println("____________________________________________________________");
-            System.out.println(userInput);
-            System.out.println("____________________________________________________________");
+            // If the user types "list", display the stored tasks
+            if (userInput.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                // Add the new task to the list
+                if (taskCount < tasks.length) {
+                    tasks[taskCount] = userInput;  // Store the task
+                    taskCount++;  // Increment the task count
+                    System.out.println("____________________________________________________________");
+                    System.out.println("added: " + userInput);
+                    System.out.println("____________________________________________________________");
+                } else {
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Task list is full. Cannot add more tasks.");
+                    System.out.println("____________________________________________________________");
+                }
+            }
         }
 
         scanner.close();
