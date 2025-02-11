10196d6c9fa32022a081606275368c0264ca9f41
LeThiHongMinh <hongminhle2005@gmail.com>
2025-02-11 13:43:49 +0800

    Handle exception

--- src/main/java/Neura.java
+++ src/main/java/Neura.java
@@ -33,8 +33,13 @@ public class Neura {
                 printGoodbye();
                 break;
             }
-
-            handleCommand(userInput);
+            try {
+                handleCommand(userInput);
+            } catch (NeuraException e) {
+                System.out.println(SEPARATOR);
+                System.out.println("OOPS!!! " + e.getMessage());
+                System.out.println(SEPARATOR);
+            }
         }
 
         scanner.close();
@@ -53,7 +58,7 @@ public class Neura {
         System.out.println(SEPARATOR);
     }
 
-    private void handleCommand(String userInput) {
+    private void handleCommand(String userInput) throws NeuraException{
         if (userInput.equals(LIST_COMMAND)) {
             displayTasks();
         } else if (userInput.startsWith(MARK_COMMAND)) {
@@ -67,14 +72,8 @@ public class Neura {
         } else if (userInput.startsWith(EVENT_COMMAND)) {
             addEventTask(userInput);
         } else {
-            printCustomMessage(userInput);
+            throw new NeuraException("I'm sorry, but I don't know what that means :-(");
         }
-    }
-
-    private void printCustomMessage(String message) {
-        System.out.println(SEPARATOR);
-        System.out.println(message);
-        System.out.println(SEPARATOR);
     }
 
     private void displayTasks() {
@@ -86,24 +85,32 @@ public class Neura {
         System.out.println(SEPARATOR);
     }
 
-    private void markTaskAsDone(String userInput) {
+    private void markTaskAsDone(String userInput) throws NeuraException{
         int taskIndex = getTaskIndex(userInput);
         if (isValidTaskIndex(taskIndex)) {
             tasks[taskIndex].markAsDone();
             printTaskStatus("Nice! I've marked this task as done:", taskIndex);
+        } else {
+            throw new NeuraException("Invalid task number.");
         }
     }
 
-    private void unmarkTaskAsDone(String userInput) {
+    private void unmarkTaskAsDone(String userInput) throws NeuraException{
         int taskIndex = getTaskIndex(userInput);
         if (isValidTaskIndex(taskIndex)) {
             tasks[taskIndex].markAsNotDone();
             printTaskStatus("OK, I've marked this task as not done yet:", taskIndex);
+        } else {
+            throw new NeuraException("Invalid task number.");
         }
     }
 
-    private int getTaskIndex(String userInput) {
-        return Integer.parseInt(userInput.split(" ")[1]) - 1;  // Get task index from user input
+    private int getTaskIndex(String userInput) throws NeuraException{
+        try {
+            return Integer.parseInt(userInput.split(" ")[1]) - 1;
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            throw new NeuraException("Invalid task number format.");
+        }
     }
 
     private boolean isValidTaskIndex(int index) {
@@ -117,7 +124,10 @@ public class Neura {
         System.out.println(SEPARATOR);
     }
 
-    private void addTodoTask(String userInput) {
+    private void addTodoTask(String userInput) throws NeuraException{
+        if (userInput.trim().equals(TODO_COMMAND)) {
+            throw new NeuraException("The description of a todo cannot be empty.");
+        }
         if (taskCount < MAX_TASKS) {
             tasks[taskCount++] = new Todo(userInput);  // Create a new Todo task
             printTaskAddedMessage();
@@ -126,26 +136,37 @@ public class Neura {
         }
     }
 
-    private void addDeadlineTask(String userInput) {
-        if (taskCount < MAX_TASKS) {
-            String taskDetails = userInput.split(" ", 2)[1];
+    private void addDeadlineTask(String userInput) throws NeuraException{
+        try {
+            String taskDetails = userInput.substring(9).trim();
             String[] details = taskDetails.split(" /by ", 2);
-            tasks[taskCount++] = new Deadline(details[0], details[1]);  // Create new Deadline task
-            printTaskAddedMessage();
-        } else {
-            printTaskListFullMessage();
+            if (details.length < 2) throw new NeuraException("Invalid deadline format. Use: deadline [task] /by [time]");
+            if (taskCount < MAX_TASKS) {
+                tasks[taskCount++] = new Deadline(details[0], details[1]);
+                printTaskAddedMessage();
+            } else {
+                printTaskListFullMessage();
+            }
+        } catch (IndexOutOfBoundsException e) {
+            throw new NeuraException("Invalid deadline format. Use: deadline [task] /by [time]");
         }
     }
 
-    private void addEventTask(String userInput) {
-        if (taskCount < MAX_TASKS) {
-            String taskDetails = userInput.split(" ", 2)[1];
+    private void addEventTask(String userInput) throws NeuraException{
+        try {
+            String taskDetails = userInput.substring(6).trim();
             String[] details = taskDetails.split(" /from ", 2);
-            String[] dateDetails = details[1].split(" /to", 2);
-            tasks[taskCount++] = new Event(details[0], dateDetails[0], dateDetails[1]);  // Create new Event task
-            printTaskAddedMessage();
-        } else {
-            printTaskListFullMessage();
+            if (details.length < 2) throw new NeuraException("Invalid event format. Use: event [task] /from [start] /to [end]");
+            String[] dateDetails = details[1].split(" /to ", 2);
+            if (dateDetails.length < 2) throw new NeuraException("Invalid event format. Use: event [task] /from [start] /to [end]");
+            if (taskCount < MAX_TASKS) {
+                tasks[taskCount++] = new Event(details[0], dateDetails[0], dateDetails[1]);
+                printTaskAddedMessage();
+            } else {
+                printTaskListFullMessage();
+            }
+        } catch (IndexOutOfBoundsException e) {
+            throw new NeuraException("Invalid event format. Use: event [task] /from [start] /to [end]");
         }
     }
 
---
+++ src/main/java/NeuraException.java
@@ -0,0 +1,5 @@
+class NeuraException extends Exception {
+    public NeuraException(String message) {
+        super(message);
+    }
+}
